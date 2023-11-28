d0f77cfa1a4873e0482fd53fb647afd068ae3da4
William M. <cominatyou@cominatyou.com>
2023-11-28 10:21:36 -0600

    Change button colour when dark

--- src/components/LightCard.tsx
+++ src/components/LightCard.tsx
@@ -16,7 +16,7 @@ export default function LightCard({ location, title, isCollected, imageUrl, yout
             <div className="flex flex-col gap-2">
                 <span className="text-black/40 dark:text-white/40 uppercase font-semibold text-xs">{location}</span>
                 <h1 className="text-4xl text-black dark:text-white font-semibold min-h-[6.5rem] break-words whitespace-pre-wrap max-h-[6.5rem]">{title.length <= 12 ? title.replace(" ", "\n") : title}</h1>
-                <button className="self-start text-sm justify-center text-blue-500 bg-neutral-200 dark:bg-neutral-600 px-3 py-1 rounded-full font-semibold" onClick={onButtonClick}>{isCollected ? "Remove from Collected" : "Mark as Collected"}</button>
+                <button className="self-start text-sm justify-center text-blue-500 dark:text-[#4d90ff] bg-neutral-200 dark:bg-neutral-600 px-3 py-1 rounded-full font-semibold" onClick={onButtonClick}>{isCollected ? "Remove from Collected" : "Mark as Collected"}</button>
             </div>
         </div>
     )
